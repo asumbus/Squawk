2bb9b26f09eb5adda1697ba3611fa6c51e5c1356
asumbus <54859135+asumbus@users.noreply.github.com>
2019-09-06 15:27:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='squawk',
+setup(name='Squawk',
       version='1.0',
       description='ASUM AVA',
       url='http://github.com/asumbus/squawk',
-      author='squawk',
+      author='Squawk',
       author_email='asum@example.com',
       license='none',
-      packages=['squawk'],
+      packages=['Squawk'],
       zip_safe=False)
