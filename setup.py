bfec3be5a91037d9d8ff809b7ae318e82951945e
asumbus <54859135+asumbus@users.noreply.github.com>
2019-09-06 15:26:36 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='squawk',
+      version='1.0',
+      description='ASUM AVA',
+      url='http://github.com/asumbus/squawk',
+      author='squawk',
+      author_email='asum@example.com',
+      license='none',
+      packages=['squawk'],
+      zip_safe=False)
